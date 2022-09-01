a89a0fa50d3cc7c66f455b3dd39ed6f52da43b5b
Hadeel Wadia <hadeelwadia@gmail.com>
2022-09-01 21:22:15 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { createElement } from "react";
+import {BrowserRouter} from 'react-dom'
+import { JsxElement } from "typescript";
 import Header from "./Components/header";
 import "./styles.css";
 
-export default function App() {
-  return  createElement('div',null,`hadeel i love you`)
-}
-  /* (
-    <div className="App">
-      <Header name="hadeel" age={33}  />
-      
-    </div>
-  );
-}
-*/
+export default function App():JsxElement {
+  return (<div className="App">
+           <Header name="hadeel" age={33}  />
+           <BrowserRouter>
+           </BrowserRouter>
+       </div>)
+    
+    }
+    
+    
+
+
+
 
 
